3e5426e57a2630774db20a682dd849a11a0e1262
Hugo Petament <h.petament@icloud.com>
2023-08-26 17:54:03 +0200

    fix

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,8 +5,8 @@ class ContactsController < ApplicationController
     def create
         @contact = Contact.new(contact_params)
         if @contact.save
-            ContactMailer.send_contact_email(@contact).deliver_now
             ContactMailer.send_contact_confirmation_email(@contact).deliver_now
+            ContactMailer.send_contact_email(@contact).deliver_now
             redirect_to root_path, notice: 'Votre message a été envoyé.'
         else
             render :new
