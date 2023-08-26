fde35ba4cc018119022198d392aefa130bae3d4c
Hugo Petament <h.petament@icloud.com>
2023-08-26 13:56:59 +0200

    test spammability

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,6 +6,7 @@ class ContactsController < ApplicationController
         @contact = Contact.new(contact_params)
         if @contact.save
             ContactMailer.send_contact_email(@contact).deliver_now
+            ContactMailer.send_contact_confirmation_email(@contact).deliver_now
             redirect_to root_path, notice: 'Votre message a été envoyé.'
         else
             render :new
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -6,4 +6,10 @@ class ContactMailer < ApplicationMailer
         attachments.inline['logo-min.png'] = File.read(Rails.root.join('app', 'assets', 'images', 'logo-min.png'))
         mail(to: 'contact@hpwebsolutions.fr', subject: 'Nouvelle demande de contact')
     end
+
+    def send_contact_confirmation_email(contact)
+        @contact = contact
+        attachments.inline['logo-min.png'] = File.read(Rails.root.join('app', 'assets', 'images', 'logo-min.png'))
+        mail(to: 'test-vxltmb1ze@srv1.mail-tester.com', subject: 'Votre demande de contact')
+    end
 end
