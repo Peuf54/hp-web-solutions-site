fbb7ba85f262d071afc575573f748ef53ad54666
Hugo Petament <h.petament@icloud.com>
2023-08-26 18:09:25 +0200

    try with 2 ovh addresses

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,11 +3,11 @@ class ContactMailer < ApplicationMailer
     
     def send_contact_email(contact)
         @contact = contact
-        mail(to: 'mecougiffadde-5335@yopmail.com', subject: 'Nouvelle demande de contact')
+        mail(to: 'hugo@hpwebsolutions.fr', subject: 'Nouvelle demande de contact')
     end
 
     def send_contact_confirmation_email(contact)
         @contact = contact
-        mail(to: 'mecougiffadde-5335@yopmail.com', subject: 'Votre demande de contact')
+        mail(to: 'contact@hpwebsolutions.fr', subject: 'Votre demande de contact')
     end
 end
