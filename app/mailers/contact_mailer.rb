e2c6654355e94d82f4cf36b01b9e7900e742dd0f
Hugo Petament <h.petament@icloud.com>
2023-08-26 14:28:04 +0200

    debug email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -10,6 +10,6 @@ class ContactMailer < ApplicationMailer
     def send_contact_confirmation_email(contact)
         @contact = contact
         attachments.inline['logo-min.png'] = File.read(Rails.root.join('app', 'assets', 'images', 'logo-min.png'))
-        mail(to: contact.email, subject: 'Votre demande de contact')
+        mail(to: @contact.email, subject: 'Votre demande de contact')
     end
 end
