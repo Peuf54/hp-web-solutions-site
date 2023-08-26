5179329898fe4fcd70f4e2c1830e057337c970c0
Hugo Petament <h.petament@icloud.com>
2023-08-26 22:35:00 +0200

    temporary removed the confirmation mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,11 +3,11 @@ class ContactMailer < ApplicationMailer
     
     def send_contact_email(contact)
         @contact = contact
-        mail(to: 'hugo@hpwebsolutions.fr', subject: 'Nouvelle demande de contact')
+        mail(to: 'contact@hpwebsolutions.fr', subject: 'Nouvelle demande de contact')
     end
 
-    def send_contact_confirmation_email(contact)
-        @contact = contact
-        mail(to: 'contact@hpwebsolutions.fr', subject: 'Votre demande de contact')
-    end
+    # def send_contact_confirmation_email(contact)
+    #     @contact = contact
+    #     mail(to: 'contact@hpwebsolutions.fr', subject: 'Votre demande de contact')
+    # end
 end
