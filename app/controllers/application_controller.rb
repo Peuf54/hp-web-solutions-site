b5bdbd27606f9d49c146578d4423144d3da4489f
Hugo Petament <h.petament@icloud.com>
2023-08-26 03:27:58 +0200

    added cache directive

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    before_action :set_cache_headers
+
+    private
+
+    def set_cache_headers
+        response.headers["Cache-Control"] = "public, max-age=3600"
+    end
 end
