f5360d4e0176453372dd1f0a65a1ab4c6634ee92
Hugo Petament <h.petament@icloud.com>
2023-08-30 23:50:30 +0200

    removed the empty div before 600px

--- app/javascript/controllers/responsive_image_controller.js
+++ app/javascript/controllers/responsive_image_controller.js
@@ -1,9 +1,6 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "stimulus";
 
-// Connects to data-controller="responsive-image"
 export default class extends Controller {
-  static targets = ["imageContainer"];
-
   connect() {
     this.checkScreenSize();
     window.addEventListener("resize", () => this.checkScreenSize());
@@ -14,10 +11,21 @@ export default class extends Controller {
   }
 
   checkScreenSize() {
+    // Supprimer l'ancienne image s'il y en a une
+    const oldImage = this.element.querySelector(".responsive-image");
+    if (oldImage) {
+      oldImage.remove();
+    }
+
+    // Ajouter une nouvelle image si la largeur de la fenêtre est supérieure à 600
     if (window.innerWidth > 600) {
-      this.imageContainerTarget.innerHTML = `<img src="https://i.ibb.co/hFw6KPb/hero-illustration.webp" alt="Illustration de création de site web." width="250" height="166">`;
-    } else {
-      this.imageContainerTarget.innerHTML = '';
+      const newImage = document.createElement("img");
+      newImage.className = "responsive-image";  // Ajouter une classe pour pouvoir supprimer cette image plus tard
+      newImage.src = "https://i.ibb.co/hFw6KPb/hero-illustration.webp";
+      newImage.alt = "Illustration de création de site web.";
+      newImage.width = 250;
+      newImage.height = 166;
+      this.element.appendChild(newImage);
     }
   }
 }
